d24c44d80dac752973b54e7f50af2305953e4868
therunningagent <luistjr@gmail.com>
2021-03-01 15:55:01 -0700

    ride serializer updated

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,4 +1,5 @@
 class RideSerializer < ActiveModel::Serializer
   # has_many :itineraries, through: :ride_itineraries
+  has_many :ride_itineraries 
   attributes :id, :name, :img, :description, :land
 end
