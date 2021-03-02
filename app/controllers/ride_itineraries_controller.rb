f18f3dd76e8af58fc1e0172b66d2ba036ce703e8
therunningagent <luistjr@gmail.com>
2021-03-02 10:24:54 -0700

    modified ride itineraries controller

--- app/controllers/ride_itineraries_controller.rb
+++ app/controllers/ride_itineraries_controller.rb
@@ -3,4 +3,9 @@ class RideItinerariesController < ApplicationController
         rideitinerary = RideItinerary.all
         render json: rideitinerary
     end 
+
+    def show 
+        rideitinerary = RideItinerary.find(params[:id])
+        render json: rideitinerary
+    end 
 end
