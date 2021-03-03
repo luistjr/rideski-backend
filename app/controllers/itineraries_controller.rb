652cef2b1db225fecb7afc5e9680a18443840439
therunningagent <luistjr@gmail.com>
2021-03-03 09:20:25 -0700

    updated create method in itineraries controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -8,4 +8,16 @@ class ItinerariesController < ApplicationController
         itinerary = Itinerary.find(params[:id])
         render json: itinerary
     end 
+
+    def create
+        itinerary = Itinerary.create(itinerary_params)
+        render json: itinerary 
+    end
+
+    private
+
+    def itinerary_params
+        params.require(:itinerary).permit(:date)
+    end
+
 end
