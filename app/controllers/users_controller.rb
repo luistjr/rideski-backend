27c6cce45d4ec400bd0cc2b321412651257978c3
therunningagent <luistjr@gmail.com>
2021-03-03 17:43:08 -0700

    added :user to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,6 @@ class UsersController < ApplicationController
     private
 
     def user_params 
-        params.require(:user).permit(:first_name, :last_name, :email, :img, :username, :password)
+        params.permit(:user, :first_name, :last_name, :email, :img, :username, :password)
     end 
 end
